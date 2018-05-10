f854ab30bb73e3035c6f3cd6d9617a583e039689
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-05-10 11:22:43 -0500

    GET in entry router that returns all entries

--- server/routes/entries.router.js
+++ server/routes/entries.router.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
+const pool = require('../modules/pool');
+
+router.get('/', (req, res) => {
+  pool.query(`
+    SELECT * FROM "entries";
+  `)
+    .then((results) => {
+      res.send(results.rows);
+    })
+    .catch((error) => {
+      console.log('Error in GET /entries:', error);
+      res.sendStatus(500);
+    })
+})
 
 module.exports = router;
